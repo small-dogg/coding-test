997c3eaef691d1033cfa8e710593804f69f95f59
small-dogg <sy920112@naver.com>
2023-09-13 23:34:22 +0900

    feat: 동적프로그래밍 : 피보나치 메모이제이션으로 호출 수 감소

--- src/main/java/codingtest/book/동적프로그래밍/피보나치예시.java
+++ src/main/java/codingtest/book/동적프로그래밍/피보나치예시.java
@@ -1,17 +1,23 @@
 package codingtest.book.동적프로그래밍;
 
+import java.util.Arrays;
+
 public class 피보나치예시 {
 
+    private static final long[] mem = new long[101];
     private static int calls = 0;
 
     public static void main(String[] args) {
+        Arrays.fill(mem, -1);
         System.out.println(fibonacci(10));
         System.out.println(calls);
     }
 
     private static long fibonacci(int n) {
         calls++;
+
+        if (mem[n] != -1) return mem[n];
         if (n == 0 || n == 1) return n;
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return mem[n] = fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
