7db6b42493b3f3863bcceabf8364d6b54f6b9478
small-dogg <sy920112@naver.com>
2023-10-06 22:25:16 +0900

    feat: leetCode: Find the index of the first occurrence in a String

---
+++ src/main/java/codingtest/leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package codingtest.leetcode;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
