70e47c620920249b07f9184e7b64e9957cd80af8
small-dogg <sy920112@naver.com>
2023-10-09 22:15:59 +0900

    feat: leetCode: ClimbingStairs(개선)

--- src/main/java/codingtest/leetcode/ClimbingStairs.java
+++ src/main/java/codingtest/leetcode/ClimbingStairs.java
@@ -5,10 +5,6 @@ public class ClimbingStairs {
     public int climbStairs(int n) {
         if(n==1 || n== 2) return n;
 
-        int[] arr = new int[46];
-        arr[1] = 1;
-        arr[2] = 2;
-
         return climbStairs(n-1) + climbStairs(n-2);
     }
 }
