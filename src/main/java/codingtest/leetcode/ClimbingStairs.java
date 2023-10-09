b90ba37df8f8fb8c007d7f1ce6027c6b82f9bbc6
small-dogg <sy920112@naver.com>
2023-10-09 22:18:46 +0900

    feat: leetCode: ClimbingStairs(개선:메모이제이션)

--- src/main/java/codingtest/leetcode/ClimbingStairs.java
+++ src/main/java/codingtest/leetcode/ClimbingStairs.java
@@ -1,10 +1,23 @@
 package codingtest.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClimbingStairs {
     // n계의 계단을 올라야하고, 한번에 1 또는 2 계단만 오를 수 있음. 몇가지 밥법으로 오를 수 있는가.
     public int climbStairs(int n) {
-        if(n==1 || n== 2) return n;
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
 
-        return climbStairs(n-1) + climbStairs(n-2);
     }
+
+    private int climbStairs(int n, Map<Integer, Integer> memo) {
+        if(n==1 || n== 2) return n;
+        if(!memo.containsKey(n)){
+            memo.put(n, climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
+        }
+        return memo.get(n);
+    }
+
+
 }
