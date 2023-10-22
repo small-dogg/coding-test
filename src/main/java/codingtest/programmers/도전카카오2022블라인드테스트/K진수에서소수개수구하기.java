5175617505069943170f9668a2a2dd34c85cac80
small-dogg <sy920112@naver.com>
2023-10-23 00:42:36 +0900

    feat: programmers: 도전 카카오 2022 블라인드 테스트 : K진수에서 소수 개수 구하기

---
+++ src/main/java/codingtest/programmers/도전카카오2022블라인드테스트/K진수에서소수개수구하기.java
@@ -0,0 +1,51 @@
+package codingtest.programmers.도전카카오2022블라인드테스트;
+
+//92335
+public class K진수에서소수개수구하기 {
+
+    private boolean isPrime(long x){
+        if(x<= 1) return false;
+
+        for (long i = 2; i * i <=x; i++) {
+            if(x%i == 0) return false;
+        }
+        return true;
+    }
+    public int solution(int n, int k) {
+        int result = 0;
+
+        String[] string = Integer.toString(n, k).split("0+");
+        for (String s : string) {
+            if(isPrime(Integer.parseInt(s))){
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
+
+//class Solution {
+//    public int solution(int n, int k) {
+//        int answer = 0;
+//        String s = Integer.toString(n, k);
+//        String[] strArr = s.split("0");
+//        for(String str:strArr){
+//            if(!str.equals("")){
+//                long val = Long.parseLong(str);
+//                if(val>1&&isPrime(Long.parseLong(str))){
+//                    answer++;
+//                }
+//            }
+//        }
+//        return answer;
+//    }
+//    private static boolean isPrime(long n) {
+//        for (int i = 2; i <= (long) Math.sqrt(n); i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
