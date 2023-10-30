3b9c029af6ca1aab391015e1fe5103f2eb97a5cd
small-dogg <sy920112@naver.com>
2023-10-31 01:18:45 +0900

    feat: leetcode: BalancedBinaryTree(진행중)

---
+++ src/main/java/codingtest/leetcode/BalancedBinaryTree.java
@@ -0,0 +1,26 @@
+package codingtest.leetcode;
+
+public class BalancedBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+
+    }
+}
