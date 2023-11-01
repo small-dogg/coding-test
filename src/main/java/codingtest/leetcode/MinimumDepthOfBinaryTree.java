55f2522f81f2ae216ab7cbecce37f5f358dbacbc
small-dogg <sy920112@naver.com>
2023-11-01 22:45:05 +0900

    feat: leetcode: MinimumDepth Of Binary Tree

---
+++ src/main/java/codingtest/leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+package codingtest.leetcode;
+
+public class MinimumDepthOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        if (root.left == null || root.right == null)
+            return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
