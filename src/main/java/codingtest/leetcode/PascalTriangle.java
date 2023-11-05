7adffc59424c5f4561379486101e42ae2a41a685
small-dogg <sy920112@naver.com>
2023-11-05 10:16:20 +0900

    feat: leetcode: Pascal's Triangle

---
+++ src/main/java/codingtest/leetcode/PascalTriangle.java
@@ -0,0 +1,34 @@
+package codingtest.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        List<List<Integer>> generate = new PascalTriangle().generate(5);
+        System.out.println("debug");
+    }
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
+        }
+
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+
+        prevRows.add(newRow);
+        return prevRows;
+    }
+}
