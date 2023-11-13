62d594e464c7d1e7998c792a2f82279191dc6bed
small-dogg <sy920112@naver.com>
2023-11-13 21:05:51 +0900

    feat: leetcode: BinaryTree Postorder Traversal

---
+++ src/main/java/codingtest/leetcode/BinaryTreePostorderTraversal.java
@@ -0,0 +1,49 @@
+package codingtest.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        if(root.left == null && root.right == null) return Arrays.asList(root.val);
+
+        List<Integer> result = new ArrayList<>();
+        result.add(root.val);
+        preOrder(root, result);
+
+        return result;
+    }
+
+    public void preOrder(TreeNode node, List<Integer> list) {
+        if(node.left!=null){
+            preOrder(node.left, list);
+            list.add(node.left.val);
+        }
+        if(node.right!=null){
+            preOrder(node.right, list);
+            list.add(node.right.val);
+        }
+
+    }
+}
