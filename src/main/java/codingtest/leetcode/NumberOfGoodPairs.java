4f107ba364d068734d0c3718d270fe30f19771fb
small-dogg <sy920112@naver.com>
2023-11-15 01:42:25 +0900

    feat: leetcode: Number Of Good Pairs

---
+++ src/main/java/codingtest/leetcode/NumberOfGoodPairs.java
@@ -0,0 +1,11 @@
+package codingtest.leetcode;
+
+public class NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int ans = 0, cnt[] = new int[101];
+        for (int a: nums) {
+            ans += cnt[a]++;
+        }
+        return ans;
+    }
+}
