1a7a2382e979bf1ea44ec490c1d49e241887157e
small-dogg <sy920112@naver.com>
2023-11-17 00:32:16 +0900

    feat: leetcode: Convert The Temperature

---
+++ src/main/java/codingtest/leetcode/ConvertTheTemperature.java
@@ -0,0 +1,8 @@
+package codingtest.leetcode;
+
+public class ConvertTheTemperature {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+
+    }
+}
